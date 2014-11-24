d1a70744d9c11d1087be3336934fbd5882c42aee
zachm1995 <zachmumbauer@gmail.com>
2014-11-24 16:27:29 -0500

    new changes

---
+++ Ai2048.java
@@ -0,0 +1,18 @@
+public class Ai2048
+{
+	static void playLeft(TwoThousandFourtyEight board)
+   {
+		while (board.left())
+      {
+			
+		}
+	}
+	static void playRandom(TwoThousandFourtyEight board)
+   {
+      
+	}
+	static void playAI(TwoThousandFourtyEight board)
+   {
+      
+	}
+}
--- TwoThousandFourtyEight.java
+++ TwoThousandFourtyEight.java
@@ -1,45 +1,56 @@
+import java.util.*;
 public class TwoThousandFourtyEight
 {
    private int score;
    private int hightile;
+   private int[][] board;
+   public TwoThousandFourtyEight()
+   {
+   }
+   public TwoThousandFourtyEight(int x, int y)
+   {
+      board =  TwoDimensional2048.blankBoard(x,y);
+   }
    public int getScore()
    {
       int tempscore = 0;
-      for(int row = 0; row < board.length(); row++)
       {
-         for(int col = 0; col < board[row].length(); col++)
+         for(int row = 0; row < board.length(); row++)
          {
-            switch(board[row][col])
+            for(int col = 0; col < board[row].length(); col++)
             {
-               case 2:
-                  tempscore += 2;
-                  break;
-               case 4:
-                  tempscore += 4+2;
-                  break;
-               case 8:
-                  tempscore += 8+4+2;
-                  break;
-               case 16:
-                  tempscore += 16+8+4+2;
-                  break;
-               case 32:
-                  tempscore += 32+16+8+4+2;
-                  break;
-               case 64:
-                  tempscore += 64+32+16+8+4+2;
-                  break;
-               case 128:
-                  tempscore += 128+64+32+16+8+4+2;
-                  break;
-               case 256:
-                  tempscore += 256+128+64+32+16+8+4+2;
-                  break;
-               case 512:
-                  tempscore += 512+256+128+64+32+16+8+4+2;
-                  break;                 
+               switch(board[row][col])
+               {
+                  case 2:
+                     tempscore += 2;
+                     break;
+                  case 4:
+                     tempscore += 4+2;
+                     break;
+                  case 8:
+                     tempscore += 8+4+2;
+                     break;
+                  case 16:
+                     tempscore += 16+8+4+2;
+                     break;
+                  case 32:
+                     tempscore += 32+16+8+4+2;
+                     break;
+                  case 64:
+                     tempscore += 64+32+16+8+4+2;
+                     break;
+                  case 128:
+                     tempscore += 128+64+32+16+8+4+2;
+                     break;
+                  case 256:
+                     tempscore += 256+128+64+32+16+8+4+2;
+                     break;
+                  case 512:
+                     tempscore += 512+256+128+64+32+16+8+4+2;
+                     break;                 
+               }
             }
-         }
+         }   
       }
       score = tempscore;
       return score;
@@ -51,7 +62,7 @@ public class TwoThousandFourtyEight
       {
          for(int col = 0; col < board[row].length(); col++)
          {
-            if (board[row][col])
+            if (board[row][col] > temp)
             {
                temp = board[row][col];
             }
@@ -63,6 +74,12 @@ public class TwoThousandFourtyEight
    public TwoThousandFourtyEight copy()
    {
       TwoThousandFourtyEight copy = new TwoThousandFourtyEight();
+      int[][] copyboard = new int[board.length][];
+      for (int i = 0; i < board.length; i++)
+      {
+        copyboard[i] = Arrays.copyOf(board[i], board[i].length);
+      }
+      copy.board = copyboard;
       return copy;
    }
    public boolean up()
@@ -78,10 +95,6 @@ public class TwoThousandFourtyEight
       
    }
    public boolean right()
-   {
-      
-   }
-   public TwoThousandFourtyEight(int x, int y)
    {
       
    }
